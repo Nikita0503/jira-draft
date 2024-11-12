8edaf97b7b3edb289dab860cc8bf4d32ac2c836e
Shevtsiv Nikita <nikita0503ua@gmail.com>
2024-11-12 14:43:41 +0200

    create comment

--- src/pages/CommentEditorPage/index.tsx
+++ src/pages/CommentEditorPage/index.tsx
@@ -1,8 +1,10 @@
-import AttachedFile from '@components/AttachedFile';
+import NewFileList from '@components/lists/NewFileList';
+import FilePicker from '@components/pickers/FilePicker';
+import useComments from '@hooks/useComments';
 import { IFile } from '@interfaces';
 import { Button, TextField } from '@mui/material';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import styles from './CommentEditorPage.module.css';
 
 const FILES: IFile[] = [
@@ -19,9 +21,50 @@ const FILES: IFile[] = [
 ];
 
 const CommentEditorPage = () => {
+  const { projectId, taskId } = useParams();
+
+  const [message, setMessage] = React.useState<string>('');
+  const [files, setFiles] = React.useState<File[]>([]);
+
+  const { loading, createComment } = useComments(
+    parseInt(projectId!),
+    parseInt(taskId!)
+  );
+
   const navigate = useNavigate();
 
+  const addFile = React.useCallback(
+    (file: File) => {
+      setFiles((uploadedFiles: File[]) => {
+        const isSelectedFile = files.some(
+          (uploadedFile: File) => uploadedFile.name === file.name
+        );
+        if (isSelectedFile) {
+          alert('You have already selected a file with this name');
+          return uploadedFiles;
+        }
+        return [...uploadedFiles, file];
+      });
+    },
+    [files]
+  );
+
+  const deleteFile = React.useCallback(
+    (file: File) => {
+      setFiles((uploadedFiles: File[]) =>
+        uploadedFiles.filter(
+          (uploadedFile: File) => uploadedFile.name !== file.name
+        )
+      );
+    },
+    [files]
+  );
+
   const createNewComment = React.useCallback(() => {
+    createComment(message, files, goToTaskDetails);
+  }, [message, files]);
+
+  const goToTaskDetails = React.useCallback(() => {
     navigate(-1);
   }, []);
 
@@ -33,22 +76,23 @@ const CommentEditorPage = () => {
           className={styles.textField}
           label="Message"
           variant="filled"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
         />
-        <span className={styles.fileListTitle}>Files</span>
+        <span className={styles.fileListTitle}>New Files</span>
         <div className={styles.fileList}>
-          {FILES.map((file: IFile) => (
-            <div key={file.id} className={styles.fileContainer}>
-              <AttachedFile file={file} />
-              <Button className={styles.buttonDeleteFile} variant="contained">
-                Delete
-              </Button>
-            </div>
-          ))}
+          <NewFileList files={files} deleteFile={deleteFile} />
         </div>
+        <FilePicker setFile={addFile}>
+          <Button className={styles.buttonAddFile} variant="contained">
+            Add File
+          </Button>
+        </FilePicker>
         <Button
           onClick={createNewComment}
           className={styles.button}
           variant="contained"
+          disabled={loading}
         >
           Create New Comment
         </Button>
